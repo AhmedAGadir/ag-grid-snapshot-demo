3e16b774d126d82c4f6d9dbb41659b41edae7e7f
AhmedAGadir <ahmed.gadir@gmail.com>
2021-05-11 09:56:27 +0100

    update

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # AG Grid Snapshot Demo
 
-This is a short demo to highlight difficulties in running test snapshots when using custom cell renderers with AG Grid React.
-
 To try it out:
 
 ```
@@ -13,6 +11,4 @@ then:
 ```
 npm test
 ```
-
-Note that if you uncomment the wait (`await new Promise((r) => setTimeout(r, 1000))`), the test will fail, because the custom cell renderer hasn't actually rendered to the DOM before the snapshot is run.
 # ag-grid-snapshot-demo
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,52 +1,5 @@
-
-// you probably want to have some sort of timeout here so this doesn't hang in the event of some
-// other grid related issue
-// const ensureGridApiHasBeenSet = (component) => {
-//   return new Promise(function (resolve, reject) {
-//     (function waitForGridReady() {
-//       if (getByTestId(component.container, 'api').innerHTML !== '') {
-//         return resolve();
-//       }
-//       setTimeout(waitForGridReady, 100);
-//     })();
-//   });
-// };
-
-
-// describe('row selection', () => {
-//   let appComponent = null;
-
-//   beforeEach(async () => {
-//     appComponent = render(<App />);
-//     await wait(() => ensureGridApiHasBeenSet(appComponent));
-//   });
-
-//   afterEach(() => {
-//     appComponent.unmount();
-//   })
-
-//   test('all rows selected', () => {
-//     let selectAllButton = getByTestId(appComponent.container, 'selectAll');
-//     fireEvent.click(selectAllButton);
-//     let selectedNodes = appComponent.container.querySelectorAll('.ag-center-cols-container .ag-row-selected');
-//     expect(selectedNodes.length).toBe(3);
-//   });
-
-//   test('all rows deselected', () => {
-//     let deSelectAllButton = getByTestId(appComponent.container, 'deSelectAll');
-//     fireEvent.click(deSelectAllButton);
-//     let selectedNodes = appComponent.container.querySelectorAll('.ag-center-cols-container .ag-row-selected');
-//     expect(selectedNodes.length).toBe(0);
-//   });
-
-// });
-
-// **********
-
-
 import "@testing-library/jest-dom/extend-expect";
 import React from "react";
-// import { render } from "@testing-library/react";
 import { render, waitFor } from '@testing-library/react';
 import { getByTestId, queryAllBy, fireEvent } from '@testing-library/dom';
 import App from "../App";
@@ -66,9 +19,6 @@ test("AG Grid renders the table", async () => {
   const { container } = render(<App />);
 
   await waitFor(() => ensureGridApiHasBeenSet(container));
-
-  // the test only passes if we wait a bit
-  // await new Promise((r) => setTimeout(r, 1000));
 
   // run an expectation on what the DOM looks like
   expect(container).toMatchInlineSnapshot(`
